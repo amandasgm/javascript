76177972acd73c8fa25b26a33d87967414bfcb62
amandasgm <144074360+amandasgm@users.noreply.github.com>
2023-10-15 17:59:13 -0300

    .

--- AULA 12/ex011.js
+++ AULA 12/ex011.js
@@ -1,11 +1,6 @@
-var idade = 66
-console.log(`Você tem ${idade} anos.`)
-if (idade < 16) {
-    console.log('Não vota')
-}else{
-    if(idade >= 16 && idade < 18 || idade >= 65 ){
-        console.log('Voto opcional')
-    }else{
-        console.log('Voto obrigátorio')
-    }   
+var idade = 8
+if(idade < 18){
+    console.log('Menor de Idade')
+} else {
+    console.log('Maior de idade')
 }
