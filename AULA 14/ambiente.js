ee0c9bcedc5efd8aacb055d7a1c8465c485ca23d
amandasgm <144074360+amandasgm@users.noreply.github.com>
2023-10-22 17:29:31 -0300

    .

--- AULA 14/ambiente.js
+++ AULA 14/ambiente.js
@@ -1,20 +1,5 @@
-/*
-//while
-var c = 1
-while (c <= 5) {
+console.log('Vai começar...')
+for(var c = 1; c <= 4; c++ ){
     console.log(c)
-    c++
 }
-
-//do while
-var c = 1
-do {
-    console.log(c)
-    c++
-} while (c <= 5) 
-*/
-
-//for
-for(var c = 1; c <= 5;   ){
-
-}
+console.log('fim')
