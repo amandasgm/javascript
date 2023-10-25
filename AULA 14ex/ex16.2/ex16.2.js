ace54601d206df96889aaa2c96890047c807df87
amandasgm <144074360+amandasgm@users.noreply.github.com>
2023-10-24 21:05:15 -0300

    .

---
+++ AULA 14ex/ex16.2/ex16.2.js
@@ -0,0 +1,19 @@
+function contar(){
+    var i = document.getElementById('iinicio')
+    var f = document.getElementById('ifim')
+    var p = document.getElementById('ipasso')
+    var msg = document.getElementById('msg')
+
+    if(i.value.length == 0 || f.value.length == 0 || p.value.length == 0){
+        msg.innerHTML = 'Dados incompletos. Preencha todas as informações pedidas.'
+    }else {
+        msg.innerHTML = `Deu tudo certo!`
+
+        var i = Number(i.value)
+        var i = Number(i.value)
+        var i = Number(i.value)
+
+        msg.
+
+    }
+}
---
+++ AULA 14ex/ex16/script.js
@@ -0,0 +1,23 @@
+function contar(){
+    var i = document.getElementById('iinicio')
+    var f = document.getElementById('ifim')
+    var p = document.getElementById('ipasso')
+    var msg = document.getElementById('msg')
+
+    if(i.value.length == 0 || f.value.length == 0 || p.value.length == 0){
+        alert('ERRO!')
+    }else{
+        msg.innerHTML = 'Contando... '
+
+        var i = Number(i.value)
+        var f = Number(f.value)
+        var p = Number(p.value)
+
+        for(var c = i; c <= f; c += p){ 
+            msg.innerHTML += `${c} `
+        }
+    }
+ 
+}
+
+
